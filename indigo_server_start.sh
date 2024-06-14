3509e6082bf89f7879ef89e82465199226b8d8e2
sglushkov <stoyan.glushkov@gmail.com>
2024-06-14 18:56:40 +0000

    first upload

---
+++ indigo_server_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+killall indigo_server
+/home/indigo/INDIGO/indigo/build/bin/indigo_server indigo_ccd_asi indigo_ccd_playerone indigo_mount_ioptron indigo_wheel_asi indigo_focuser_mypro2_tony 
+
+
+
